354dbbec1bb4f947b9524467afa5f178222e5651
Luis Farfan <lucho.farfan9@gmail.com>
2018-02-20 01:55:07 -0500

    estructura de ejemplo, se podria cambiar de acuerdo al proyecto

---
+++ src/app/modulo-ejemplo/componente-ejemplo/componente-ejemplo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-ejemplo',
+  templateUrl: './componente-ejemplo.component.html',
+  styleUrls: ['./componente-ejemplo.component.css']
+})
+export class ComponenteEjemploComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modulo-ejemplo/modulo-ejemplo.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponenteEjemploComponent } from './componente-ejemplo/componente-ejemplo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ComponenteEjemploComponent]
+})
+export class ModuloEjemploModule { }
